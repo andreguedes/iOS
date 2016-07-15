8ce4344b7799cb60a1cadab20e26386822d0c749
andresguedes <andresguedes@live.com>
2016-07-14 22:59:03 -0300

    Playground com variaveis e tipos

---
+++ Course iOS9/MyPlayground.playground/Contents.swift
@@ -0,0 +1,32 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//String
+var str = "Hello, playground"
+var name:String = "André Guedes"
+print("Hello \(name)")
+print("Hello " + name + ".")
+
+//Integer
+var int:Int = 9
+int = int * 2
+int = int / 2
+
+var anotherInt = 7
+print(int * anotherInt)
+print("The value of int is \(int)")
+
+//Decimal
+var number:Double = 8.3
+print(number * Double(int))
+
+//Boolean
+var isMale:Bool = true
+
+
+//Exercise
+var intValue:Int = 2
+var doubleValue:Double = 3.2
+
+print("Multiplicacao entre \(intValue) por \(doubleValue) é \(Double(intValue) * doubleValue)")
